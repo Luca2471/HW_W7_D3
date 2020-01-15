4eb1c41bd6531778cc2dafd9d25ba6f80e4b0b38
Luca Sanz Charreun <lucasanz2471@gmail.com>
2020-01-15 20:43:41 +0000

    added a drop down list that is not working and a countryselector

---
+++ src/components/CountriesList.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div>
+    <ul>
+      <list-item v-for="(country, index) in countries"
+      :country="country" :key="index"></list-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ListItem from './ListItem.vue';
+
+export default {
+  name: 'countries-list',
+  props: ['countries'],
+  components:{
+    "list-item": ListItem
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
+
+
+<select id="countries-list" v-model="selectedCountry">
+  <list-item disabled value="">Choose a Country :)</list-item>
+  <list-item v-for="country in countries" :value="country">{{ country.name }}</list-item>
+</select>
